f03b696bbf722a7f7868463261ef56e96ed3722c
OLIVER D SANCHEZ <trickquestionis@gmail.com>
2019-03-11 19:06:54 -0400

    Added http interceptor and authentication service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -17,6 +18,7 @@ import { LoginComponent } from './pages/login/login.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/core/http/interceptors/post-interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { PostInterceptor } from './post-interceptor';
+
+describe('PostInterceptor', () => {
+  it('should create an instance', () => {
+    expect(new PostInterceptor()).toBeTruthy();
+  });
+});
